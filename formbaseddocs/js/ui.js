36dd88b3da3ef54474ee59195ee66737a6a34c61
juancamejoalarcon <juancamejo93@hotmail.com>
2019-06-27 07:13:12 +0200

    venga

---
+++ formbaseddocs/js/ui.js
@@ -0,0 +1,25 @@
+class UI {
+
+    constructor() {}
+
+    subMenuNavigation(e) {
+        e.preventDefault();
+
+        document.querySelectorAll('.sub-menu__nav__button').forEach((button) => {
+            if (button.classList.contains('sub-menu__nav__button--current')) {
+                const content = document.querySelector('#' + button.getAttribute('data-toogle'));
+                content.classList.add('lateral-ease-outro');
+                content.classList.remove('lateral-ease-intro');
+                setTimeout(function(){ content.style.display = 'none';}, 400);
+            }
+            button.classList.remove('sub-menu__nav__button--current');
+        });
+
+        e.target.classList.add('sub-menu__nav__button--current');
+        const content = document.querySelector('#' + e.target.getAttribute('data-toogle'));
+        content.classList.remove('lateral-ease-outro');
+        content.classList.add('lateral-ease-intro');
+        setTimeout(function(){ content.style.display = 'block';}, 400);
+
+    }
+}
