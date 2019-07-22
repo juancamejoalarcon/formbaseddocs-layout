9e67eb1fee00c1ddb038f6f932bbb5460d985eb6
juancamejoalarcon <juancamejo93@hotmail.com>
2019-07-22 14:09:15 +0200

    cden

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
-function search() {
-    console.log('prueba');
+// Modal 
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
 }
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
